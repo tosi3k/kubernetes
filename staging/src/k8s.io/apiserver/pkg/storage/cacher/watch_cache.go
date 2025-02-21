066534f527f0492d40767d58a75393fb5931a8b9
Antoni Zawodny <zawodny@google.com>
2025-02-21 15:07:01 +0100

    Add watch cache capacity upper bound adjusting logic

--- staging/src/k8s.io/apiserver/pkg/storage/cacher/watch_cache.go
+++ staging/src/k8s.io/apiserver/pkg/storage/cacher/watch_cache.go
@@ -169,7 +169,7 @@ func newWatchCache(
 		getAttrsFunc:        getAttrsFunc,
 		cache:               make([]*watchCacheEvent, defaultLowerBoundCapacity),
 		lowerBoundCapacity:  defaultLowerBoundCapacity,
-		upperBoundCapacity:  defaultUpperBoundCapacity,
+		upperBoundCapacity:  capacityUpperBound(eventFreshDuration),
 		startIndex:          0,
 		endIndex:            0,
 		store:               newStoreIndexer(indexers),
@@ -187,6 +187,25 @@ func newWatchCache(
 	wc.indexValidator = wc.isIndexValidLocked
 
 	return wc
+}
+
+// capacityUpperBound denotes the maximum possible capacity of the watch cache
+// to which it can resize.
+func capacityUpperBound(eventFreshDuration time.Duration) int {
+	if eventFreshDuration <= DefaultEventFreshDuration {
+		return defaultUpperBoundCapacity
+	}
+	// eventFreshDuration determines how long the watch events are supposed
+	// to be stored in the watch cache.
+	// In very high churn situations, there is a need to store more events
+	// in the watch cache, hence it would have to be upsized accordingly.
+	// Because of that, for larger values of eventFreshDuration, we set the
+	// upper bound of the watch cache's capacity proportionally to the ratio
+	// between eventFreshDuration and DefaultEventFreshDuration.
+	// Given that the watch cache size can only double, we round up that
+	// proportion to the next power of two.
+	exponent := int(math.Ceil((math.Log2(eventFreshDuration.Seconds() / DefaultEventFreshDuration.Seconds()))))
+	return defaultUpperBoundCapacity << exponent
 }
 
 // Add takes runtime.Object as an argument.
